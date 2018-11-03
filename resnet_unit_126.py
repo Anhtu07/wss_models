6360174688e71312de363b8878a02b3e30d9eeb8
Tu Nguyen <tu.nguyenanh.96@gmail.com>
2018-11-03 12:08:08 +0700

    unit

---
+++ resnet_unit_126.py
@@ -0,0 +1,264 @@
+import pandas as pd
+import os
+from PIL import Image
+import numpy as np
+from scipy import ndimage
+from scipy import misc
+from keras.callbacks import ModelCheckpoint
+from random import shuffle
+import glob
+import os
+
+
+image_size = (224, 224, 3)
+
+print("Suffle, Decrease Learning Rate, Checkpoint")
+print("Reading csv file ...")
+
+
+csvfile = pd.read_csv('./all_products_3m_with_images.csv')
+
+
+
+
+cmap = {}
+pmap = {}
+j = 0
+for i in range(len(csvfile.ProductID)):
+    pid = csvfile.ProductID[i]
+    cid = csvfile.CategoryID[i]
+    if cid not in cmap:
+        cmap.update({cid : j})
+        j += 1
+    if pid not in pmap:
+        pmap.update({pid : cmap[cid]})
+
+print("Spliting data into train, validation, test")
+
+
+
+shuffle_data = True  # shuffle the addresses before saving
+path = './imgs_3m/'  # address to where you want to save
+
+# read addresses and labels from the 'train' folder
+addrs = os.listdir(path)
+labels = [pmap[int(filename.split('_')[0])] for filename in addrs]
+
+if shuffle_data:
+    c = list(zip(addrs, labels))
+    shuffle(c)
+    addrs, labels = zip(*c)
+
+# Divide the hata into 60% train, 20% validation, and 20% test
+train_addrs = addrs[0:int(0.7*len(addrs))]
+train_labels = labels[0:int(0.7*len(labels))]
+val_addrs = addrs[int(0.7*len(addrs)):]
+val_labels = labels[int(0.7*len(addrs)):]
+
+
+print("Done")
+
+
+import numpy as np
+import keras
+print("Creating DataGenerator ...")
+
+class DataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, list_IDs, labels, batch_size=20, dim=(224,224), n_channels=3,
+                 n_classes=126, shuffle=False):
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation__(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation__(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size), dtype=int)
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            # Store sample
+            img = Image.open(path + ID)
+            img.load()
+            img = misc.imresize(img, image_size)
+            data = np.asarray(img, dtype='int32')
+            X[i,] = data / 255
+
+            # Store class
+            y[i] = self.labels[i]
+
+        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+
+
+training_generator = DataGenerator(train_addrs, train_labels)
+validation_generator = DataGenerator(val_addrs, val_labels)
+
+from keras import layers
+from keras import models
+
+
+#
+# image dimensions
+#
+
+img_height = 224
+img_width = 224
+img_channels = 3
+
+#
+# network params
+#
+
+cardinality = 16
+
+
+def residual_network(x):
+    """
+    ResNeXt by default. For ResNet set `cardinality` = 1 above.
+
+    """
+    def add_common_layers(y):
+        y = layers.BatchNormalization()(y)
+        y = layers.LeakyReLU()(y)
+
+        return y
+
+    def grouped_convolution(y, nb_channels, _strides):
+        # when `cardinality` == 1 this is just a standard convolution
+        if cardinality == 1:
+            return layers.Conv2D(nb_channels, kernel_size=(3, 3), strides=_strides, padding='same')(y)
+
+        assert not nb_channels % cardinality
+        _d = nb_channels // cardinality
+
+        # in a grouped convolution layer, input and output channels are divided into `cardinality` groups,
+        # and convolutions are separately performed within each group
+        groups = []
+        for j in range(cardinality):
+            group = layers.Lambda(lambda z: z[:, :, :, j * _d:j * _d + _d])(y)
+            groups.append(layers.Conv2D(_d, kernel_size=(3, 3), strides=_strides, padding='same')(group))
+
+        # the grouped convolutional layer concatenates them as the outputs of the layer
+        y = layers.concatenate(groups)
+
+        return y
+
+    def residual_block(y, nb_channels_in, nb_channels_out, _strides=(1, 1), _project_shortcut=False):
+        """
+        Our network consists of a stack of residual blocks. These blocks have the same topology,
+        and are subject to two simple rules:
+        - If producing spatial maps of the same size, the blocks share the same hyper-parameters (width and filter sizes).
+        - Each time the spatial map is down-sampled by a factor of 2, the width of the blocks is multiplied by a factor of 2.
+        """
+        shortcut = y
+
+        # we modify the residual building block as a bottleneck design to make the network more economical
+        y = layers.Conv2D(nb_channels_in, kernel_size=(1, 1), strides=(1, 1), padding='same')(y)
+        y = add_common_layers(y)
+
+        # ResNeXt (identical to ResNet when `cardinality` == 1)
+        y = grouped_convolution(y, nb_channels_in, _strides=_strides)
+        y = add_common_layers(y)
+
+        y = layers.Conv2D(nb_channels_out, kernel_size=(1, 1), strides=(1, 1), padding='same')(y)
+        # batch normalization is employed after aggregating the transformations and before adding to the shortcut
+        y = layers.BatchNormalization()(y)
+
+        # identity shortcuts used directly when the input and output are of the same dimensions
+        if _project_shortcut or _strides != (1, 1):
+            # when the dimensions increase projection shortcut is used to match dimensions (done by 1×1 convolutions)
+            # when the shortcuts go across feature maps of two sizes, they are performed with a stride of 2
+            shortcut = layers.Conv2D(nb_channels_out, kernel_size=(1, 1), strides=_strides, padding='same')(shortcut)
+            shortcut = layers.BatchNormalization()(shortcut)
+
+        y = layers.add([shortcut, y])
+
+        # relu is performed right after each batch normalization,
+        # expect for the output of the block where relu is performed after the adding to the shortcut
+        y = layers.LeakyReLU()(y)
+
+        return y
+
+    # conv1
+    x = layers.Conv2D(32, kernel_size=(7, 7), strides=(2, 2), padding='same')(x)
+    x = add_common_layers(x)
+
+    # conv2
+    x = layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same')(x)
+    for i in range(2):
+        project_shortcut = True if i == 0 else False
+        x = residual_block(x, 64, 128, _project_shortcut=project_shortcut)
+
+    # conv3
+    for i in range(2):
+        # down-sampling is performed by conv3_1, conv4_1, and conv5_1 with a stride of 2
+        strides = (2, 2) if i == 0 else (1, 1)
+        x = residual_block(x, 128, 256, _strides=strides)
+
+    # conv4
+    for i in range(3):
+        strides = (2, 2) if i == 0 else (1, 1)
+        x = residual_block(x, 256, 512, _strides=strides)
+
+    # conv5
+    for i in range(2):
+        strides = (2, 2) if i == 0 else (1, 1)
+        x = residual_block(x, 512, 1024, _strides=strides)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Dense(126, activation = 'softmax')(x)
+
+    return x
+
+
+image_tensor = layers.Input(shape=(img_height, img_width, img_channels))
+network_output = residual_network(image_tensor)
+
+model = models.Model(inputs=[image_tensor], outputs=[network_output])
+print(model.summary())
+
+filepath="/warehouse/weights_resnet_unit_126.best.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+callbacks_list = [checkpoint]
+
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adam(lr=0.001, decay = 0.95),
+              metrics=['accuracy'])
+
+
+
+model.fit_generator(generator=training_generator,
+                    validation_data=validation_generator,
+                    epochs = 60, callbacks=callbacks_list, verbose=1)
