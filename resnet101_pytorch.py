c488698293e05eca93ff14bfd0ef351d5665f83c
Tu Nguyen <tu.nguyenanh.96@gmail.com>
2018-11-30 16:19:46 +0700

    resnet101

---
+++ resnet101_pytorch.py
@@ -0,0 +1,101 @@
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from collections import OrderedDict
+import time
+
+data_dir = './images_category'
+train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize([185.78795853, 183.04217856, 182.72907307],
+                                                            [69.36995286, 70.42220844, 70.29462924])])
+
+test_transforms = transforms.Compose([transforms.Resize(255),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([185.78795853, 183.04217856, 182.72907307],
+                                                           [69.36995286, 70.42220844, 70.29462924])])
+
+# Pass transforms in here, then run the next cell to see how the transforms look
+train_data = datasets.ImageFolder(data_dir + '/train', transform=train_transforms)
+test_data = datasets.ImageFolder(data_dir + '/test', transform=test_transforms)
+
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
+testloader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle=True)
+
+model = models.resnet101(pretrained=True)
+
+for param in model.parameters():
+    param.requires_grad = False
+
+for param in model.layer4.parameters():
+    param.requires_grad = True
+
+from collections import OrderedDict
+classifier = nn.Sequential(OrderedDict([
+                          ('fc1', nn.Linear(2048, 800)),
+                          ('relu', nn.ReLU()),
+                          ('dropout', nn.Dropout(0.2)),
+                          ('fc2', nn.Linear(800, 120)),
+                          ('output', nn.LogSoftmax(dim=1))
+                          ]))
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model.fc = classifier
+criterion = nn.NLLLoss()
+
+# Only train the classifier parameters, feature parameters are frozen
+optimizer = optim.Adam(model.fc.parameters(), lr=0.001, decay = 1e-4)
+model.to(device);
+
+epochs = 30
+steps = 0
+running_loss = 0
+batch_num = 0
+for epoch in range(epochs):
+    for inputs, labels in trainloader:
+        steps += 1
+        batch_num += 1
+        # Move input and label tensors to the default device
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        logps = model.forward(inputs)
+        loss = criterion(logps, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    test_loss = 0
+    accuracy = 0
+    model.eval()
+    with torch.no_grad():
+        for inputs, labels in testloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            logps = model.forward(inputs)
+            batch_loss = criterion(logps, labels)
+
+            test_loss += batch_loss.item()
+
+            # Calculate accuracy
+            ps = torch.exp(logps)
+            top_p, top_class = ps.topk(1, dim=1)
+            equals = top_class == labels.view(*top_class.shape)
+            accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+    print(f"Epoch {epoch+1}/{epochs}.. "
+          f"Train loss: {running_loss/print_every:.3f}.. "
+          f"Test loss: {test_loss/len(testloader):.3f}.. "
+          f"Test accuracy: {accuracy/len(testloader):.3f}")
+    running_loss = 0
+    model.train()
+
+    if accuracy/len(testloader) > 0.75:
+        torch.save(model.state_dict(), '/warehouse/weight_resnet101_pytorch.pth')
